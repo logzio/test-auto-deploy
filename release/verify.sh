99c5034cd3709fd3939d98933e0fd87e4e9d156b
MarinaRazumovsky <marina@logz.io>
2018-02-19 12:19:20 +0200

    add project

--- release/verify.sh
+++ release/verify.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo '-----------------TRAVIS_TAG-------------------'
 echo $TRAVIS_TAG
-echo @GPG_KEY
-./mvnw verify -Drevision=$TRAVIS_TAG -Ggpg_key=$GPG_KEY -Dgpg.skip
+echo $GPG_KEY
+./mvnw verify -Drevision=$TRAVIS_TAG -Ggpg_key=$GPG_KEY -Dgpg.passphrase=$GPG_PASSPHRASE
 echo '---------------------------------------------'
