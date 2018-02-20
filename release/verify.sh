a3b42cc1e80d4c23d0345ecafaec50bca1dd8e66
MarinaRazumovsky <marina@logz.io>
2018-02-20 11:39:26 +0200

    add project

--- release/verify.sh
+++ release/verify.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-echo '-----------------TRAVIS_TAG-------------------'
-#gpg --fast-import release/keys
-echo $TRAVIS_TAG
-echo $GPGKEY
-./mvnw verify -Drevision=$TRAVIS_TAG -Dgpg.passphrase=$GPG_PASSPHRASE
-echo '---------------------------------------------'
+
+openssl aes-256-cbc -k $CODESIGNING -in release/codesigning.asc.enc -out release/codesigning.asc -d
+gpg --fast-import release/codesigning.asc
+echo 'Release version '$TRAVIS_TAG
+./mvnw verify -Drevision=$TRAVIS_TAG -Dgpg_key=$GPG_KEY -Dgpg.passphrase=$GPG_PASSPHRASE
+rm release/codesigning.asc
