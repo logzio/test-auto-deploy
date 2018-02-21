edcabe7265266fefb10a03e9ce6b909bef7e7704
MarinaRazumovsky <marina@logz.io>
2018-02-21 13:15:28 +0200

    add project

--- release/verify.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-echo 'prepare private key to import'
-openssl aes-256-cbc  -in release/codesigning.asc.enc -out release/codesigning.asc -d -k $CODESIGNING
-gpg --fast-import release/codesigning.asc
-echo 'Release version '$TRAVIS_TAG
-./mvnw verify -Drevision=$TRAVIS_TAG -Dgpg_key=$GPG_KEY -hgpg.passphrase=$GPG_PASSPHRASE
-rm release/codesigning.asc
